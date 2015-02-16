260065dc4ba21be3288ccf5d52473c81314cdeb8
Marc R <3rafanan@gmail.com>
2015-02-15 16:32:49 -0800

    Created AppWindow and Scene
    Signed-off-by: Marc R <3rafanan@gmail.com>

---
+++ Source/Engine/AppWindow.cpp
@@ -0,0 +1,102 @@
+#include "AppWindow.h"
+
+/******************************************************************************
+*	FUNCTION: gameLoop
+*
+*	DATE: February 15, 2015
+*
+*	REVISIONS: (Date and Description)
+*
+*	DESIGNER: Marc Rafanan
+*
+*	PROGRAMMER: Marc Rafanan
+*
+*	INTERFACE: gameLoop()
+*
+*	PARAMETERS:
+*		// void for now
+*
+*	RETURNS:
+*		bool 	- success or failure
+*
+*	NOTES:
+*		The main loop for the game. The game loop processes turns for all of 
+*		the scenes. Each scene is loaded into the app window on creation and 
+*		will not have a loop of its own for the sake of synchronisation.
+*
+******************************************************************************/
+bool AppWindow::gameLoop()
+{
+	while(true)
+	{
+			//for(vector<Scene
+			//ProcessEvents
+			//Update
+			//Draw
+	}
+	return true;
+}
+
+/******************************************************************************
+*	FUNCTION: addScene
+*
+*	DATE: February 15, 2015
+*
+*	REVISIONS: (Date and Description)
+*
+*	DESIGNER: Marc Rafanan
+*
+*	PROGRAMMER: Marc Rafanan
+*
+*	INTERFACE: bool addScene(Scene scene)
+*
+*	PARAMETERS:
+*		scene	- scene to be added to the appwindow
+*
+*	RETURNS:
+*		int		- scene id
+*
+*	NOTES:
+*		Adds a scene to the app window and returns the scene id.
+*
+******************************************************************************/
+int AppWindow::addScene(Scene* scene)
+{
+	this->scenes.push_back(scene);
+	
+	// return id of the scene added. used for removeScene. Either return
+	// id after adding or overload == operator for the scene.
+	return (this->scenes.size() - 1);
+}
+
+
+/******************************************************************************
+*	FUNCTION: removeScene
+*
+*	DATE: February 15, 2015
+*
+*	REVISIONS: (Date and Description)
+*
+*	DESIGNER: Marc Rafanan
+*
+*	PROGRAMMER: Marc Rafanan
+*
+*	INTERFACE: bool removeScene(int index)
+*
+*	PARAMETERS:
+*		index	- index of the scene to be removed
+*
+*	RETURNS:
+*		void
+*
+*	NOTES:
+*		Removes a scene from the scenes vector in the app window
+*
+******************************************************************************/
+bool AppWindow::removeScene(int index)
+{
+	this->scenes.erase(this->scenes.begin() + index);
+	
+	return true;
+}
+
---
+++ Source/Engine/Scene.cpp
@@ -0,0 +1,176 @@
+#include "Scene.h"
+
+/******************************************************************************
+*	FUNCTION: Scene
+*
+*	DATE: February 15, 2015
+*
+*	REVISIONS: (Date and Description)
+*
+*	DESIGNER: Marc Rafanan
+*
+*	PROGRAMMER: Marc Rafanan
+*
+*	INTERFACE: Scene()
+*
+*	PARAMETERS:
+*		// void for now
+*
+*	RETURNS:
+*		void
+*
+*	NOTES:
+*		Scene constructor
+*
+******************************************************************************/
+Scene::Scene()
+{
+	// What makes up a scene
+}
+
+/******************************************************************************
+*	FUNCTION: onLoad
+*
+*	DATE: February 15, 2015
+*
+*	REVISIONS: (Date and Description)
+*
+*	DESIGNER: Marc Rafanan
+*
+*	PROGRAMMER: Marc Rafanan
+*
+*	INTERFACE: onLoad()
+*
+*	PARAMETERS:
+*		// void for now
+*
+*	RETURNS:
+*		bool	- success or failure
+*
+*	NOTES:
+*		logic for the scene on loading
+*
+******************************************************************************/
+bool Scene::onLoad()
+{
+}
+
+/******************************************************************************
+*	FUNCTION: unLoad
+*
+*	DATE: February 15, 2015
+*
+*	REVISIONS: (Date and Description)
+*
+*	DESIGNER: Marc Rafanan
+*
+*	PROGRAMMER: Marc Rafanan
+*
+*	INTERFACE: unLoad()
+*
+*	PARAMETERS:
+*		// void for now
+*
+*	RETURNS:
+*		void
+*
+*	NOTES:
+*		logic for the scene un loading
+*
+******************************************************************************/
+bool Scene::unLoad()
+{
+}
+
+/******************************************************************************
+*	FUNCTION: events
+*
+*	DATE: February 15, 2015
+*
+*	REVISIONS: (Date and Description)
+*
+*	DESIGNER: Marc Rafanan
+*
+*	PROGRAMMER: Marc Rafanan
+*
+*	INTERFACE: events()
+*
+*	PARAMETERS:
+*		// void for now
+*
+*	RETURNS:
+*		void
+*
+*	NOTES:
+*		logic for getting all events on a scene
+*
+******************************************************************************/
+void Scene::events()
+{
+}
+
+/******************************************************************************
+*	FUNCTION: update
+*
+*	DATE: February 15, 2015
+*
+*	REVISIONS: (Date and Description)
+*
+*	DESIGNER: Marc Rafanan
+*
+*	PROGRAMMER: Marc Rafanan
+*
+*	INTERFACE: update()
+*
+*	PARAMETERS:
+*		// void for now
+*
+*	RETURNS:
+*		void
+*
+*	NOTES:
+*		Updates the scene
+*
+******************************************************************************/
+void Scene::update()
+{
+}
+
+
+/******************************************************************************
+*	FUNCTION: draw
+*
+*	DATE: February 15, 2015
+*
+*	REVISIONS: (Date and Description)
+*
+*	DESIGNER: Marc Rafanan
+*
+*	PROGRAMMER: Marc Rafanan
+*
+*	INTERFACE: draw()
+*
+*	PARAMETERS:
+*		// void for now
+*
+*	RETURNS:
+*		void
+*
+*	NOTES:
+*		draws the scene to the screen
+*
+******************************************************************************/
+void Scene::draw()
+{
+}
+
+int main(void)
+{
+	// This is just a test main for scene class
+	Scene testScene;
+	
+	return 0;
+}
+
+
+
