0b59cc997bdab04bfba6161fc97a4d2b536b1e5c
Melvin Loho <mloho@outlook.com>
2015-02-17 23:22:39 -0800

    Added all Game Object source code, also cleaned up and renamed some things.

--- Source/Multimedia/assets/maps/readme.txt
+++
@@ -1,2 +0,0 @@
-TileSet (.tset) files list the texture sub rectangle specifications of each tile.
-TiledMap (.tmap) files will use the information from a TileSet file to place them accordingly.
---
+++ Source/Multimedia/gameobject/SGO.cpp
@@ -0,0 +1,50 @@
+#include "SGO.h"
+#include "Batch.h"
+
+/**
+* Sprite Game Object.
+*
+* A BGO that represents a sprite.
+*
+* @author Melvin Loho
+*/
+
+SGO::SGO() {}
+
+SGO::SGO(const sf::Texture& texture)
+{
+	data.setTexture(texture);
+}
+
+SGO::SGO(const sf::Texture& texture, const sf::IntRect& rect)
+{
+	data.setTexture(texture);
+	data.setTextureRect(rect);
+}
+
+SGO::~SGO() {}
+
+sf::Sprite& SGO::operator()()
+{
+	return data;
+}
+const sf::Sprite& SGO::operator()() const
+{
+	return data;
+}
+
+const sf::Transform& SGO::getLocalTransform() const {
+	return data.getTransform();
+}
+
+void SGO::middleAnchorPoint(bool arg) {
+	if (arg) {
+		const sf::FloatRect &bounds = data.getLocalBounds();
+		data.setOrigin(bounds.width * 0.5f, bounds.height * 0.5f);
+	}
+	else data.setOrigin(0.f, 0.f);
+}
+
+void SGO::draw(Batch& batch, sf::RenderStates states) const {
+	batch.draw(*this, states);
+}
