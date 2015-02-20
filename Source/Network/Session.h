16eaadf7fcb3547c6e2a182894b551ae44ef1f55
calvinrempel <calvinrempel@users.noreply.github.com>
2015-02-19 20:50:45 -0800

    Created Session.h
    Defined main interface for interacting with sessions.

---
+++ Source/Network/Session.h
@@ -0,0 +1,53 @@
+/*-----------------------------------------------------------------------------
+-- SOURCE FILE: Session.h - The header file for Networking Sessions.
+--
+-- CLASS: Session
+--
+-- DATE: February, 17, 2015
+--
+-- REVISIONS: -
+--
+-- DESIGNER: Calvin Rempel
+--
+-- PROGRAMMER: Calvin Rempel
+--
+-- NOTES:
+-- A Session is used to read and write messages to a connected socket through
+-- a supplied ReadProcess and SendProcess. Messages received are handled
+-- locally if they relate to the connection itself, or through the
+-- supplied NetworkEntityMultiplexer if the message relates to
+-- NetworkEntities.
+-----------------------------------------------------------------------------*/
+
+#ifndef _NETWORK_SESSION_H_
+#define _NETWORK_SESSION_H_
+
+namespace Networking
+{
+	class socket;
+	class ReadProcess;
+	class SendProcess;
+	struct Message;
+	class NetworkEntityMultiplexer;
+
+	class Session
+	{
+		public:
+			Session(socket *socket, ReadProcess *readProcess, SendProcess *sendProcess, NetworkEntityMultiplexer *entityMux);
+			virtual ~Session();
+			
+			void send(Message *message);
+			void disconnect();
+
+		private:
+			void onMessageReceived(Message *message);
+			void onConnectionClosedByRemote();
+			
+			socket *socket;
+			ReadProcess *readProcess;
+			SendProcess *sendProcess;
+			NetworkEntityMultiplexer *entityMux;
+	};
+}
+
+#endif
