e1554c7b25c74a94fac9872b8babe18b206420bb
Calvin Rempel <rempel.calvin@gmail.com>
2015-03-04 00:07:11 -0800

    Added Session implementation. I think it should compile, but I haven't tried.

---
+++ Source/Network/Session.cpp
@@ -0,0 +1,44 @@
+#include "Session.h"
+
+#include "ReceiveProcess"
+#include "SendProcess"
+#include "NetworkEntityMultiplexer.h"
+#include "Message.h"
+
+#include <sys/socket.h>
+
+Networking::Session::Session(unsigned int socket, ReadProcess *readProcess, SendProcess *sendProcess, NetworkEntityMultiplexer *entityMux)
+	: socket(socket), readProcess(readProcess), sendProcess(sendProcess), entityMux(entityMux)
+{
+	// Nothing to do
+}
+
+Networking::Session::~Session()
+{
+	disconnect();
+}
+
+unsigned int Networking::Session::getSocket()
+{
+	return socket;
+}
+
+void Networking::Session::send(Message *message)
+{
+	sendProcess->send(message);
+}
+
+void Networking::Session::disconnect()
+{
+	close(socket);
+}
+
+void onMessageReceived(Message *message)
+{
+	entityMux->onMessage(this, message);
+}
+
+void onConnectionClosedByRemote()
+{
+	// Handle Closed Connections
+}
