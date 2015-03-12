4d830c533846cb3be7792695e8d03aaf516c1621
Eric Tsang <surplus.et@gmail.com>
2015-03-11 23:26:35 -0700

    changed indentation to spaces
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- Source/Network/SendProcess.h
+++ Source/Network/SendProcess.h
@@ -17,50 +17,50 @@
 
 namespace Networking
 {
-	struct Message;
-	class Session;
+    struct Message;
+    class Session;
 
-	enum SendMessageType
-	{
-		ADD_SOCKET = 0,
-		REMOVE_SOCKET = 1,
-		SEND_MESSAGE = 2,
-		SHUTDOWN = 3
-	};
+    enum SendMessageType
+    {
+        ADD_SOCKET = 0,
+        REMOVE_SOCKET = 1,
+        SEND_MESSAGE = 2,
+        SHUTDOWN = 3
+    };
 
-	struct SendMessage
-	{
-		SendMessageType type;
-		int socket;
-		char data[NETWORK_MESSAGE_SIZE];
-	};
+    struct SendMessage
+    {
+        SendMessageType type;
+        int socket;
+        char data[NETWORK_MESSAGE_SIZE];
+    };
 
 
-	class SendProcess
-	{
-		public:
-			SendProcess();
-			~SendProcess();
-			void sendToChild(Session *session, Message *message_s);
-			void addSession(Session *session);
-			void removeSession(Session *session);
-		
-		private:
-			void awaitFurtherInstructions();
-			void closeSockets(bool parent);
-			int sendPartial(SendMessage* m, int length);
-			void sendNetworkMessage(int socket, char* message);
-		
-			int pid;
-			int qid;
-			/*
-				sockets keeps a map of the parent int value for the socket as key
-				and the child int value for the socket as value.
-			 */
-			std::map<int, int> sockets;
-	};
+    class SendProcess
+    {
+        public:
+            SendProcess();
+            ~SendProcess();
+            void sendToChild(Session *session, Message *message_s);
+            void addSession(Session *session);
+            void removeSession(Session *session);
 
-	
+        private:
+            void awaitFurtherInstructions();
+            void closeSockets(bool parent);
+            int sendPartial(SendMessage* m, int length);
+            void sendNetworkMessage(int socket, char* message);
+
+            int pid;
+            int qid;
+            /*
+                sockets keeps a map of the parent int value for the socket as key
+                and the child int value for the socket as value.
+             */
+            std::map<int, int> sockets;
+    };
+
+
 }
 
 #endif
