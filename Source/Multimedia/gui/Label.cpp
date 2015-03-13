e0fa713d4a9aafc9b13d75b5b87f98281a91d9bd
Lewis Scott <lewisscott0@gmail.com>
2015-03-13 17:07:41 +0000

    Added my shit

--- Source/Multimedia/gui/Label.cpp
+++ Source/Multimedia/gui/Label.cpp
@@ -1,59 +1,70 @@
 
-#include "label.h"
+#include "Label.h"
 
-/**
-* Initializes a label
-*
-* @date         2015-02-27
-*
-* @revisions
-*
-* @designer   
-*
-* @programmer   Jonathan Chu
-*
-* @return       initializer
-*/
-Label::Label(BGO* parent, SGO* theBackground, std::string theText)
+namespace GUI
 {
-    parent.add(this);
-    theBackground = false;
-    background = theBackground;
-    data = theText;
-}
+	/**
+	* Initializes a label
+	*
+	* @date         2015-02-27
+	*
+	* @revisions
+	*
+	* @designer   
+	*
+	* @programmer   Jonathan Chu
+	*
+	* @return       initializer
+	*/
+	Label::Label(BGO* parent, SGO* theBackground, std::string theText)
+	{
+		parent->add(*this);
+		back_enabled = true;
+		background = theBackground;
+		add(*background);
+		
+		TGO::operator()().setString(theText);
+	}
 
-/**
-* Toggles the background between on/off
-*
-* @date         2015-02-27
-*
-* @revisions
-*
-* @designer   
-*
-* @programmer   Jonathan Chu
-*
-* @return       void
-*/
-void Label::toggleBackground(bool toggled)
-{
-    theBackground = toggled;
-}
+	/**
+	* Toggles the background between on/off
+	*
+	* @date         2015-02-27
+	*
+	* @revisions
+	*
+	* @designer   
+	*
+	* @programmer   Jonathan Chu
+	*
+	* @return       void
+	*/
+	void Label::toggleBackground(bool toggled)
+	{
+		back_enabled = toggled;
+	}
 
-/**
-* Sets the offset based on the offset given
-*
-* @date         2015-02-27
-*
-* @revisions
-*
-* @designer   
-*
-* @programmer   Jonathan Chu
-*
-* @return       void
-*/
-void Label::setOffset(sf::Vector2f theOffset)
-{
-    offset = theOffset;
+	/**
+	* Sets the offset based on the offset given
+	*
+	* @date         2015-02-27
+	*
+	* @revisions
+	*
+	* @designer   
+	*
+	* @programmer   Jonathan Chu
+	*
+	* @return       void
+	*/
+	void Label::setOffset(sf::Vector2f theOffset)
+	{
+		offset = theOffset;
+		background->operator()().move(offset);
+	}
+	
+	void Label::setText(std::string text)
+	{
+		TGO::operator()().setString(text);
+	}
 }
