8ed4e4c16d00857390ad09503d16d6b9132cec74
Melvin Loho <mloho@outlook.com>
2015-03-26 20:11:39 -0700

    HUGE UPDATE (again) - GameObject, Renderer and no more messy functionals.
    Sometimes you forget your own designs and make your classes be something it's not supposed to be. It's all good again now.
    As for the entities, they now have floating point movements.
    
    Signed-off-by: Melvin Loho <mloho@outlook.com>

--- Source/Engine/VEntity.h
+++ Source/Engine/VEntity.h
@@ -15,15 +15,13 @@ public:
 	VEntity(SGO &_sprite, Map * _map, float x, float y, Controller * ctrl, float h, float w) :
 		sprite(_sprite), Entity(_map, x, y, ctrl, h, w)
 	{
-		const sf::FloatRect &bounds = sprite().getLocalBounds();
-		sprite().setOrigin(bounds.width * 0.5f, bounds.height);
+		const sf::FloatRect &bounds = sprite.sprite().getLocalBounds();
+		sprite.sprite().setOrigin(bounds.width * 0.5f, bounds.height);
 	}
 
 	void update(const sf::Time& t) override
 	{
-		static unsigned counter = 0;
-
-		move(left + 0.1, top + 0.1, false);
+		move(left + t.asSeconds(), top + t.asSeconds(), true);
 	}
 
 protected:
