67d268d058ae7a1b078a3fdc17da0892acbcbdac
CaKlassen <ca.klassen95@gmail.com>
2015-04-06 20:27:32 -0700

    Changed wanderer companions to wisps.

--- Source/Logic/Environment/GameMap.cpp
+++ Source/Logic/Environment/GameMap.cpp
@@ -982,7 +982,7 @@ void GameMap::generateMiniBosses()
 				{
 					cell = blockMap[j][i].getRandomCell();
 
-					eh->getEnemy(&enemy, "grass/lost_grass/air_grass");
+					eh->getEnemy(&enemy, "grass/lost_grass/ground_grass");
 					gameScene->createEnemy(getEnemyType(enemy), NULL,
 						cell->getX(), cell->getY());
 				}
