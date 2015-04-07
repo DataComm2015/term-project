da6ccd6c3a161bda29bff65875750afc398a6baa
mvouve <mvouve@gmail.com>
2015-04-06 17:14:55 -0700

    Map logic fix?

--- Source/Engine/Map.cpp
+++ Source/Engine/Map.cpp
@@ -167,7 +167,7 @@ Cell* Map::getCell(const int x, const int y) const
    	unsigned int index = (x * width_) + y;
     if(y < 0 || x < 0 ||x >= width_ || y >= height_ )
     {
-    	return *cells_.end();
+    	return *(--cells_.end());
     }
 	/**/
 	try
